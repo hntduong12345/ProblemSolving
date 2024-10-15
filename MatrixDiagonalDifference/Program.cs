4bb8a0a1adeb26045f84e99b4e0a5ed1034cecac
hntduong12345 <hntduong2003@gmail.com>
2024-10-15 20:07:50 +0700

    Add project files.

---
+++ MatrixDiagonalDifference/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int result = 0;
+        int leftDia = 0, rightDia = 0;
+
+        for (int i = 0; i < arr.Count(); i++)
+        {
+            leftDia += arr[i][i];
+            rightDia += arr[i][arr[i].Count()-i-1];
+        }
+
+        Console.WriteLine(leftDia);
+        Console.WriteLine(rightDia);
+
+        Console.WriteLine(Math.Abs(leftDia - rightDia));
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>
+        {
+            new List<int>{1,2,11},
+            new List<int>{4,9,6},
+            new List<int>{-8,8,-3},
+        };
+
+        int result = Result.diagonalDifference(arr);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ TestFunction/Program.cs
@@ -0,0 +1,163 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = null;
+        ListNode l1CurNode = l1;
+        ListNode l2CurNode = l2;
+
+        bool isl1Tail = false, isl2Tail = false;
+        int sum = 0;
+        int addition = 0;
+
+        while (!isl1Tail || !isl2Tail)
+        {
+
+            if (!isl1Tail && !isl2Tail)
+            {
+                if (addition > 0)
+                {
+                    sum = l1CurNode.val + l2CurNode.val + addition;
+                }
+                else
+                {
+                    sum = l1CurNode.val + l2CurNode.val;
+                }
+
+                if (sum >= 10)
+                {
+                    sum -= 10;
+                    addition = 1;
+                }
+                else
+                {
+                    addition = 0;
+                }
+            }
+
+            if (isl1Tail && !isl2Tail)
+            {
+                if (addition > 0)
+                {
+                    sum = l2CurNode.val + addition;
+                }
+                else
+                {
+                    sum = l2CurNode.val;
+                }
+
+                if (sum >= 10)
+                {
+                    sum -= 10;
+                    addition = 1;
+                }
+                else
+                {
+                    addition = 0;
+                }
+            }
+
+            if (!isl1Tail && isl2Tail)
+            {
+                if (addition > 0)
+                {
+                    sum = l1CurNode.val + addition;
+                }
+                else
+                {
+                    sum = l1CurNode.val;
+                }
+
+                if (sum >= 10)
+                {
+                    sum -= 10;
+                    addition = 1;
+                }
+                else
+                {
+                    addition = 0;
+                }
+            }
+
+            if (result == null)
+            {
+                result = new ListNode(sum);
+            }
+            else
+            {
+                ListNode newNode = new ListNode(sum, result);
+                result = newNode;
+            }
+
+            if (l1CurNode.next == null)
+            {
+                isl1Tail = true;
+            }
+            else
+            {
+                l1CurNode = l1CurNode.next;
+            }
+
+            if (l2CurNode.next == null)
+            {
+                isl2Tail = true;
+            }
+            else
+            {
+                l2CurNode = l2CurNode.next;
+            }
+
+            if (isl1Tail && isl2Tail)
+            {
+                if (addition > 0)
+                {
+                    ListNode newNode = new ListNode(addition, result);
+                    result = newNode;
+                }
+            }
+
+        };
+
+        return result;  
+    }
+
+    public static ListNode Reverse(ListNode l)
+    {
+        ListNode temp = null, prevNode = null;
+
+        while (l != null)
+        {
+            temp = l.next;
+            l.next = prevNode;
+            prevNode = l;
+            l = temp;
+        }
+
+        return prevNode;
+    }
+
+    public static void Main(string[] args)
+    {
+        ListNode l1 = new ListNode(4, new ListNode(7));
+        ListNode l2 = new ListNode(6, new ListNode(8));
+
+        ListNode l3 = AddTwoNumbers(l1, l2);
+
+        ListNode l4 = Reverse(l3);
+
+        Console.WriteLine("Oke");
+    }
+}
+
+
