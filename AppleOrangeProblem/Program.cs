0c3e74ae94f1f99c273a9c29e66a227413538614
hntduong12345 <hntduong2003@gmail.com>
2024-10-16 07:46:21 +0700

    feat: add apples and oranges problem

--- AppleOrangeProblem/Program.cs
+++ AppleOrangeProblem/Program.cs
@@ -50,22 +50,19 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        //string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
-        //int s = Convert.ToInt32(firstMultipleInput[0]);
-        //int t = Convert.ToInt32(firstMultipleInput[1]);
-        //string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
-        //int a = Convert.ToInt32(secondMultipleInput[0]);
-        //int b = Convert.ToInt32(secondMultipleInput[1]);
-        //string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
-        //int m = Convert.ToInt32(thirdMultipleInput[0]);
-        //int n = Convert.ToInt32(thirdMultipleInput[1]);
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        int s = Convert.ToInt32(firstMultipleInput[0]);
+        int t = Convert.ToInt32(firstMultipleInput[1]);
+        string[] secondMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        int a = Convert.ToInt32(secondMultipleInput[0]);
+        int b = Convert.ToInt32(secondMultipleInput[1]);
+        string[] thirdMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+        int m = Convert.ToInt32(thirdMultipleInput[0]);
+        int n = Convert.ToInt32(thirdMultipleInput[1]);
 
-        //List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
-        //List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
+        List<int> apples = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(applesTemp => Convert.ToInt32(applesTemp)).ToList();
+        List<int> oranges = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(orangesTemp => Convert.ToInt32(orangesTemp)).ToList();
 
-        List<int> apples = new List<int> { -2, 2, 1 };
-        List<int> oranges = new List<int> { 5, -6 };
-
-        Result.countApplesAndOranges(7, 11, 5, 15, apples, oranges);
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
     }
 }
