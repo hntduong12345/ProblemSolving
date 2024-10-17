90a4508a5fb6aeea0213810cb7d659f0639ba5dc
hntduong12345 <hntduong2003@gmail.com>
2024-10-17 17:29:45 +0700

    feat: add 2 kangaroo problem

--- TwoKangarooProblem/Program.cs
+++ TwoKangarooProblem/Program.cs
@@ -11,9 +11,16 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
 
 class Result
 {
+    /*Problem:
+    You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+    The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+    The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+    You have to figure out a way to get both kangaroos at the same location at the same time as part of the show.If it is possible, return YES, otherwise return NO.*/
 
     /*
      * Complete the 'kangaroo' function below.
