e6514a480fa75b2c1566f052d6d1677e5e9d03ab
hntduong12345 <hntduong2003@gmail.com>
2024-10-22 14:09:40 +0700

    chore: change code folder location

---
+++ DivisibleSumPair/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int count = 0;
+        List<int> storage = new List<int>(new int[k]);
+
+        for (int i = 0; i < ar.Count(); i++)
+        {
+            storage[ar[i] % k]++;
+            //storage[1]++;
+        }
+
+        for (int i = 0; i <= k / 2; i++)
+        {
+            if (i == 0 || i * 2 == k)
+                count += (storage[i] * (storage[i] - 1)) / 2;
+            else
+                count += storage[i] * storage[k - i];
+        }
+
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        Console.WriteLine(result);
+    }
+}
---
+++ GCDandLCMProblem/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'getTotalX' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        int count = 0;
+
+        //Find GCD of b
+        int gcdOfB = b[0];
+        for (int i = 1; i < b.Count; i++)
+        {
+            gcdOfB = GCD(b[i], gcdOfB);
+
+            if (gcdOfB == 1)
+                break;
+        }
+
+        //Find LCD of a
+        int lcmOfA = LCM(a);
+
+        if (lcmOfA > b.Min()) return count;
+
+        for (int i = 1; i * lcmOfA <= b.Min(); i++)
+        {
+            if (gcdOfB % (lcmOfA * i) == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //Find GCD
+    public static int GCD(int a, int b)
+    {
+        if (a == 0) return b;
+
+        return GCD(b % a, a);
+    }
+
+    //Find LCD
+    public static int LCM(List<int> b)
+    {
+        return b.Aggregate((x, y) => x * y / GCD(x, y));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        Console.WriteLine(total);
+    }
+}
